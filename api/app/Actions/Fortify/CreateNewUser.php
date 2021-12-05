d834642f8fcf6c27ff6e4bdaabda429dac1c88b9
DESKTOP-E16V4PL\SAKIL <shakilreyan9397@gmail.com>
2021-12-05 20:48:32 +0600

    change crreate useer

--- api/app/Actions/Fortify/CreateNewUser.php
+++ api/app/Actions/Fortify/CreateNewUser.php
@@ -24,51 +24,7 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input)
     {
-        $db = trim($input['username']);
-        Artisan::call('config:clear');
-Artisan::call('config:cache');
-Artisan::call('cache:clear');
-        DB::statement('create database ' .$db );
-        // Config::set("database.connections.mysql", [
-        //     "host" => "127.0.0.1",
-        //     "database" => $db,
-        //     "username" => "root",
-        //     "password" => ""
-        // ]);
-        //get old dbname
-$oldDbName = env("DB_DATABASE");
-
-
-$env = new DotenvEditor();
-
-// Changes the value of the Database name and username
-$env->changeEnv([
-    'DB_DATABASE'   => $db,
-]);
-Artisan::call('config:clear');
-Artisan::call('config:cache');
-Artisan::call('cache:clear');
-DB::disconnect(env("DB_CONNECTION"));
-
-Config::set('database.connections.' . env("DB_CONNECTION"), array(
-        'driver'    => 'mysql', //or $request['driver'],
-        'host'      => "127.0.0.1",
-        'database'  => $db,
-        'username'  => 'root',
-        'password'  => '',
-        'charset'   => 'utf8',
-        'collation' => 'utf8_general_ci',
-        'prefix'    => '',
-));
-
-//Trying reconnect database
-try {
-        DB::connection()->getPdo();
-    } catch (\Exception $e) {
-        DB::disconnect(env("DB_CONNECTION"));
-        Config::set('database.connections.' . env("DB_CONNECTION") .'.database', $oldDbName );
-    }
-        Artisan::call('migrate');
+       
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'username' => ['required', 'string', 'max:255'],
