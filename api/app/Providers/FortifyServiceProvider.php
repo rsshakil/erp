69fb6d17ee0519ed73fc19885cd161ec6a1ea854
DESKTOP-E16V4PL\SAKIL <shakilreyan9397@gmail.com>
2021-12-12 08:57:39 +0600

    custom login by user phone

--- api/app/Providers/FortifyServiceProvider.php
+++ api/app/Providers/FortifyServiceProvider.php
@@ -11,6 +11,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -42,6 +44,28 @@ class FortifyServiceProvider extends ServiceProvider
 
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+        Fortify::authenticateUsing(function (Request $request) {
+            \Artisan::call('config:clear');
+            \Artisan::call('cache:clear');
+            \Artisan::call('config:cache');
+            \DB::disconnect();
+            $config = \Config::get('database.connections.mysql');
+           // print_r( $config);
+        $config['database'] = 'quickmas_'.$request->company_id;
+        \config()->set('database.connections.mysql', $config);
+        \DB::reconnect();
+        \DB::purge('mysql');
+
+        $config = \Config::get('database.connections.mysql');
+        \config(['database.default'=>$request->company_id]);
+            $user = User::where('email', $request->username)->orwhere('username', $request->username)->orwhere('phone', $request->username)->first();
+           
+            if ($user &&
+                Hash::check($request->password, $user->password)) {
+                    //return $user->createToken('token')->plainTextToken;
+                return $user;
+            }
         });
     }
 }
